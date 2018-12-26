4c3ab61f1e3b42018a4866e5f4a98b1d7e8d67aa
Kaleb Batz <kbatz@e1r3p4.21-school.ru>
2018-12-26 21:30:14 +0300

    bit

--- src/bits.c
+++ src/bits.c
@@ -6,10 +6,28 @@
 /*   By: kbatz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/25 17:58:01 by kbatz             #+#    #+#             */
-/*   Updated: 2018/12/25 19:36:23 by kbatz            ###   ########.fr       */
+/*   Updated: 2018/12/26 21:29:40 by kbatz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-char	ft_get_bit
+unsigned long	ft_gbf(void *a, size_t n, size_t len)
+{
+	size_t	res;
+	size_t	s;
+
+	res = 0;
+	c_s = sizeof(char) - 1;
+	i = n * len >> c_s;
+	p = ((n + 1) * len - 1) >> c_s;
+	start = n * len & c_s;
+	while(i <= p)
+	{
+		i++;
+	}
+
+
+
+	res = (a[n * len / s] << (8 - n * len & 7 - len)) >> (8 - len);
+}
